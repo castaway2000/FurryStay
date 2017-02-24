8f31699f02e013a312033b8f1b58053cb993743d
Adam Szablya <szablya@gmail.com>
2017-02-24 00:12:54 +0000

    fixed error with search engine and models

--- hackathon_starter/locations/models.py
+++ hackathon_starter/locations/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Location(models.Model):
+    name = models.CharField(max_length=256)
     street = models.CharField(max_length=256)
     city = models.CharField(max_length=256)
     state = models.CharField(max_length=256)
